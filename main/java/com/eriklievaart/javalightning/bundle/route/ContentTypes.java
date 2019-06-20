0ffdf0fae895c617c6dae310b20096ccdaebbe4e
Erik Lievaart <nospam@eriklievaart.com>
2019-06-20 14:25:23 +0200

    fixed: correct mime type for javascript

--- main/java/com/eriklievaart/javalightning/bundle/route/ContentTypes.java
+++ main/java/com/eriklievaart/javalightning/bundle/route/ContentTypes.java
@@ -1,14 +1,25 @@
 package com.eriklievaart.javalightning.bundle.route;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.eriklievaart.toolkit.io.api.UrlTool;
+import com.eriklievaart.toolkit.lang.api.str.Str;
 
 public class ContentTypes {
 
 	public static String getDefaultContentType(String path) {
-		String ext = UrlTool.getExtension(path);
-		if (ext != null && ext.equals("css")) {
-			return "text/css";
-		}
-		return "text/html";
+		Map<String, String> extToMime = createMimeMap();
+		String ext = Str.defaultIfEmpty(UrlTool.getExtension(path), "html").toLowerCase();
+		return extToMime.getOrDefault(ext, "text/html");
+	}
+
+	private static Map<String, String> createMimeMap() {
+		Map<String, String> extToMime = new HashMap<>();
+
+		extToMime.put("css", "text/css");
+		extToMime.put("js", "text/javascript");
+
+		return extToMime;
 	}
 }
